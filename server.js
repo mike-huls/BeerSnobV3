c794a8b5d01973324325340613683f48de57924c
unknown <m.huls@datanext.nl>
2021-05-23 18:12:15 +0200

    corrected router to app in server.js

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const http = require('http');
 const express = require('express');
-// const cors = require('cors');
 const bodyParser = require('body-parser');
-// const models = require('./models');
 const loggingService = require('./services/loggingService')
-
 const NAMESPACE = 'server';
 
 
-
-// Create router
-const router = express();
-// router.use(cors());
-
-
+// Create app
+const app = express();
 
 
 /* Logging the request */
-router.use((req, res, next) => {
+app.use((req, res, next) => {
 	loggingService.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
 
 	res.on('finish', () => {
@@ -27,15 +20,13 @@ router.use((req, res, next) => {
 });
 
 
-
 /* Parse requests */
-router.use(bodyParser.urlencoded({ extended: false }))		//routerlication/x-www-form-urlencoded
-router.use(bodyParser.json({limit: '50mb'}));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
 
 
 /* Rules of API*/
-router.use((req, res, next) => {
+app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	if (req.method == 'OPTIONS') {
@@ -46,20 +37,17 @@ router.use((req, res, next) => {
 })
 
 
-
 /* Routes */
-router.use('/api/countries', require('./routes/countries'));
-router.use('/api/cities', require('./routes/cities'));
-router.use('/api/venues', require('./routes/venues'));
-router.use('/api/beers', require('./routes/beers'));
-router.use('/api/users', require('./routes/users'));
-router.use('/api/reports', require('./routes/reports'));
-
-
+app.use('/api/countries', require('./routes/countries'));
+app.use('/api/cities', require('./routes/cities'));
+app.use('/api/venues', require('./routes/venues'));
+app.use('/api/beers', require('./routes/beers'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/reports', require('./routes/reports'));
 
 
 /* Error Handling */
-router.use((req, res, next) => {
+app.use((req, res, next) => {
 	const error = new Error('not found');
 	return res.status(404).json({
 		message: error.message
@@ -67,9 +55,8 @@ router.use((req, res, next) => {
 })
 
 
-
 /* Create Server */
-const httpServer = http.createServer(router);
+const httpServer = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => {
 	loggingService.info(NAMESPACE, `Server running on ${PORT}`)
